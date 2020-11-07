bc403316d0600c2c875c16ac11d9ed3480edfc1a
Andre Rong <andrerong23@gmail.com>
2020-11-07 23:21:01 +1100

    Make rays bounce off surfaces to approximately model Lambertian

--- main.cpp
+++ main.cpp
@@ -6,10 +6,16 @@
 #include "raytracing.h"
 #include "sphere.h"
 
-color ray_color(const ray& r, const hittable& world) {
+color ray_color(const ray& r, const hittable& world, int depth) {
     hit_record rec;
+
+    // If we've exeeded the ray bounce limit, no more light is gathered.
+    if (depth <= 0)
+        return color(0,0,0);
+
     if (world.hit(r, 0, infinity, rec)) {
-        return 0.5 * (rec.normal + color(1,1,1));
+        point3 target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1);
     }
 
     vec3 unit_direction = unit_vector(r.direction());
@@ -23,12 +29,13 @@ int main() {
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 100;
+    const int max_depth = 50;
 
     //World
     hittable_list world;
     world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
     world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
-    world.add(make_shared<sphere>(point3(-50, 25, -50), 3));
+    //world.add(make_shared<sphere>(point3(-50, 25, -50), 3));
 
     //Camera
     camera cam;
@@ -41,10 +48,10 @@ int main() {
         for (int i = 0; i < image_width; ++i) {
             color pixel_color(0, 0, 0);
             for (int s = 0; s < samples_per_pixel; ++s) {
-                auto u = (i + random_double() - 0.5) / (image_width-1);
-                auto v = (j + random_double() - 0.5) / (image_height-1);
+                auto u = (i + random_double()) / (image_width-1);
+                auto v = (j + random_double()) / (image_height-1);
                 ray r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, world);
+                pixel_color += ray_color(r, world, max_depth);
             }
             write_color(std::cout, pixel_color, samples_per_pixel);
         }
